5bddff0ee5490ac9677497c1608bd4d982916fd1
waterjin6 <waterjin326@gmail.com>
2022-02-07 22:58:35 +0900

    navigation bar restore by activity

--- app/src/main/java/com/example/haejihae/MainActivity.java
+++ app/src/main/java/com/example/haejihae/MainActivity.java
@@ -26,35 +26,24 @@ import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    Toolbar toolbar;
+    //Toolbar toolbar;
     ArrayList<SubscribeItems> myData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_main);
 
-/*
-        //아이템 추가, 데이터베이스 연동 예정
-        myData = new ArrayList<>();
-        addItem("Netflix","2022/02/08","31");
-        addItem("Wavve","2012/03/13","1");
-
-        RecyclerView recyclerView = findViewById(R.id.rv_subscribe_items);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-        SubscribeItemsAdapter adapter = new SubscribeItemsAdapter(myData,this);
-        recyclerView.setAdapter(adapter);*/
-
-
-        /*drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.toolbar);
+        //toolbar = findViewById(R.id.main_toolbar);
 
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
         //로그인 로그아웃 둘 중 하나는 숨기기
         Menu menu = navigationView.getMenu();
@@ -62,31 +51,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
         navigationView.bringToFront();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //drawerLayout.addDrawerListener(toggle);
+        //toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+
         navigationView.setCheckedItem(R.id.nav_subscribeInfo);
-        */
+
+        //아이템 추가 , 데이터베이스 연동 예정
+        myData = new ArrayList<>();
+        addItem("Netflix","2022/02/08","31");
+        addItem("Waave","2012/03/13","1");
+
+        RecyclerView recyclerView = findViewById(R.id.rv_subscribe_items);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        SubscribeItemsAdapter adapter = new SubscribeItemsAdapter(myData, this);
+        recyclerView.setAdapter(adapter);
 
 
-        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-
-        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.iv_menu).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setItemIconTintList(null);
 
-        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
-        NavigationUI.setupWithNavController(navigationView, navController);
-
-        findViewById(R.id.btn_notifications).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.iv_notifications).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(getApplicationContext(),AlarmSet.class);
@@ -95,23 +88,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         });
 
-        // - 버튼 클릭
+        // + - 버튼 클릭
+        ImageButton btn_add = findViewById(R.id.btn_add);
+
         ImageButton btn_remove = findViewById(R.id.btn_remove);
         btn_remove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 SubscribeItemsRemoveAdapter adapter = new SubscribeItemsRemoveAdapter(myData);
-                //recyclerView.setAdapter(adapter);
+                recyclerView.setAdapter(adapter);
 
                 findViewById(R.id.btn_remove_remove).setVisibility(View.VISIBLE);
                 findViewById(R.id.btn_remove_back).setVisibility(View.VISIBLE);
 
-                 /*
-                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                 SubscribeInfoRemoveFragment subscribeInfoRemoveFragment = new SubscribeInfoRemoveFragment();
-                 transaction.replace(R.id.layout_subscribe, subscribeInfoRemoveFragment);
-                 transaction.commit();*/
+                /*
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                SubscribeInfoRemoveFragment subscribeInfoRemoveFragment = new SubscribeInfoRemoveFragment();
+                transaction.replace(R.id.layout_subscribe, subscribeInfoRemoveFragment);
+                transaction.commit();*/
             }
         });
 
@@ -119,15 +114,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public void onClick(View view) {
                 SubscribeItemsAdapter adapter = new SubscribeItemsAdapter(myData, getApplicationContext());
-                //recyclerView.setAdapter(adapter);
+                recyclerView.setAdapter(adapter);
 
                 findViewById(R.id.btn_remove_remove).setVisibility(View.INVISIBLE);
                 findViewById(R.id.btn_remove_back).setVisibility(View.INVISIBLE);
             }
         });
 
+    }
 
-
+    public void itemAddClick(View v){
+        Intent intent = new Intent(this, Items_addActivity.class);
+        intent.putExtra("data","Test Popup");
+        startActivityForResult(intent, 1);
     }
 
     @Override
@@ -138,6 +137,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         else{
             super.onBackPressed();
         }
+    }
+
+    public void addItem(String name, String date, String dday){
+        SubscribeItems item = new SubscribeItems();
+
+        item.setName(name);
+        item.setDate(date);
+        item.setDday(dday);
+        myData.add(item);
     }
 
     @Override
@@ -154,14 +162,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
-
-    public void addItem(String name, String date, String dday){
-        SubscribeItems item = new SubscribeItems();
-
-        item.setName(name);
-        item.setDate(date);
-        item.setDday(dday);
-        myData.add(item);
-    }
-
 }
